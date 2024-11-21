d38e84ef79f5d2db322353c30b84d5aa26e90884
Kacper Szymczyk <157245134+kacperszymczyk72@users.noreply.github.com>
2024-11-21 09:40:07 +0100

    Zad5

---
+++ Zad5/Zad5.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+    if (n <= 0) return 0;  
+    if (n === 1) return 1; 
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        const temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+console.log(fibonacci(3));
+console.log(fibonacci(9));
+console.log(fibonacci(50));
