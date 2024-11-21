9a1976f94481c2e7da4b780843b64d579816f28a
Kacper Szymczyk <157245134+kacperszymczyk72@users.noreply.github.com>
2024-11-21 07:54:10 +0100

    zad1

--- Zad1/functions.js
+++ Zad1/functions.js
@@ -1,8 +1,10 @@
 function isOdd(x) {
-    return x % 2 != 0;
+    return x % 2 !== 0;
 }
 
-const isEven = (x) => x % 2 == 0;
+const isEven = (x) => x % 2 === 0;
 
 console.log(isOdd(3)); 
+console.log(isOdd(4));
+console.log(isEven(3)); 
 console.log(isEven(4)); 
